739e6aeaba371e4af1aa4c567b0d8171a884c9ae
Junaid <151920218+JunaidKhaleel093@users.noreply.github.com>
2024-12-01 19:57:58 +0530

    header fixed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <header className="top-0 left-0 w-full shadow-xl z-50 py-2 px-4 sm:px-8 backdrop-blur-lg bg-gradient-to-r from-green-100 to-blue-200 bg-opacity-30 dark:bg-opacity-40 rounded-md transition-all duration-200 backdrop-filter">
-
       <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           <Link to="/" className="flex items-center space-x-2 text-2xl font-bold text-[#105a63]">
@@ -14,3 +13,9 @@ export default function Header() {
             <Link to="/" className="text-gray-700 hover:text-gray-900 px-3 py-2">Home</Link>
             <Link to="/blog" className="text-gray-700 hover:text-gray-900 px-3 py-2">Blog</Link>
             <Link to="/about" className="text-gray-700 hover:text-gray-900 px-3 py-2">About</Link>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
